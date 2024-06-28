2575b6dc5642f5fe2e00e4ccbddd2ff8571b3086
Viktor <urazlov.@gmail.com>
2024-06-28 12:59:59 +0400

    add draggable wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
 "use client";
+import dynamic from "next/dynamic";
 import { Village } from "./components/Backgrounds/Village/Village";
+import { DWrapper } from "./components/Dragggable/Draggable";
 import { Spotify } from "./components/Spotify/Spotify";
+import { usePosSpotify, useSpotifyMusic } from "./store";
 
-export default function Home() {
+
+export function Home() {
+  const { isSpotifyToggled, isSpotifyShown } = useSpotifyMusic();
+
+  const { spotifyPosX, spotifyPosY, setSpotifyPos } = usePosSpotify();
+
   return (
     <div>
       <Village />
-      <Spotify />
+      <DWrapper
+        toggleHook={isSpotifyToggled && isSpotifyShown}
+        defaultX={spotifyPosX}
+        defaultY={spotifyPosY}
+        setPosition={setSpotifyPos}
+      >
+        <Spotify />
+      </DWrapper>
     </div>
   );
 }
+
+export default dynamic(() => Promise.resolve(Home), {
+  ssr: false,
+});
